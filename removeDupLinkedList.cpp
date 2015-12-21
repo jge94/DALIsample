e2ce86259bf3ba7e41594cddb8bfa86caf3cc35b
jge <ge_jinnan@wheatoncollege.edu>
2015-12-21 00:18:25 -0500

    sample 3

---
+++ removeDupLinkedList.cpp
@@ -0,0 +1,48 @@
+class Solution {
+	public:
+		ListNode *deleteDuplicates(ListNode *head) 
+		{	if (!head) 
+				return head;
+			ListNode dummy(head->val + 1);
+			dummy.next = head;
+			recur(&dummy, head);
+			return dummy.next;
+		}
+	private:
+		static void recur(ListNode *prev, ListNode *cur) 
+		{	if (cur == nullptr) 
+				return;
+			if (prev->val == cur->val) 
+			{	prev->next = cur->next;
+				delete cur;
+				recur(prev, prev->next);
+			} 
+			else 
+			{
+				recur(prev->next, cur->next);
+			}
+		}
+};
+
+
+// Second Method: Iterative
+// Remove Duplicates from Sorted List
+class Solution {
+	public:
+		ListNode *deleteDuplicates(ListNode *head) 
+		{
+			if (head == nullptr) return nullptr;
+			for (ListNode *prev = head, *cur = head->next; cur; cur = cur->next) 
+			{	if (prev->val == cur->val) 
+				{
+					prev->next = cur->next;
+					delete cur;
+				} 
+				else 
+				{
+					prev = cur;
+				}
+			}
+			return head;
+		}
+};
